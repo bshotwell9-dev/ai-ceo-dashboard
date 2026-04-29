bf69f792c4740d5325b7c565d4c0ff47db59f1f2
bshotwell9-dev <bshotwell9@gmail.com>
2026-04-28 21:03:01 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,5 @@
+import './globals.css'
+export const metadata = { title: 'AI CEO Dashboard', description: 'Autonomous Monetizer' }
+export default function RootLayout({ children }) {
+  return (<html lang="en"><body className="bg-black">{children}</body></html>)
+}
